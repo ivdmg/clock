ea76d9e94ba47e522b4db68cf025175f90189a89
Ivan <ivanganushkin71581@gmail.com>
2025-03-26 21:43:49 +0300

    add calendar and dropdown

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 const week = document.querySelector(".week");
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+const btn = document.getElementById("btn"); 
+const handHrs = document.getElementById("hand-hrs");
+const handMin = document.getElementById("hand-min")
+const handSec = document.getElementById("hand-sec")
+
+const digitalClock = document.querySelector(".clock-digital");
+const analogClock = document.querySelector(".clock-analog");
+const calendar = document.querySelector(".calendar");
+
+const digitalBtn = document.getElementById("digital");
+const analogBtn = document.getElementById("analog");
+const calendarBtn = document.getElementById("calendar");
+
+const dropdownbtn = document.getElementById("btndropdown");
+const dropdownlist = document.querySelector(".dropdown-list");
+
 function launchDigital(){
     let currentData = new Date();
 
@@ -11,17 +31,6 @@ function launchDigital(){
     sec.innerHTML = (currentData.getSeconds() < 10 ? "0" : "") + currentData.getSeconds();
     week.innerHTML = (days[currentData.getDay()].slice(0, 3))
 }
-
-// launchDigital();
-
-// setInterval(launchDigital, 1000);
-
-const btn = document.getElementById("btn");
-
-// 
-const handHrs = document.getElementById("hand-hrs");
-const handMin = document.getElementById("hand-min")
-const handSec = document.getElementById("hand-sec")
 
 function launchAnalog(){
     let date = new Date();
@@ -38,34 +47,116 @@ function launchAnalog(){
     handMin.style.transform = `translate(0, -100%) rotate(${mRotate}deg)`
     handSec.style.transform = `translate(0, -85%) rotate(${sRotate}deg)`
 }
-// launchAnalog();
-
-// setInterval(launchAnalog, 1000);
-
-const btnToggle = document.getElementById("btn");
-const digitalClock = document.querySelector(".clock-digital");
-const analogClock = document.querySelector(".clock-analog");
-
 
 let intervalAnalog;
 launchDigital();
 let intervalDigital = setInterval(launchDigital, 1000);
 function addAnalog(){
     clearInterval(intervalDigital);
-    btnToggle.innerText = "Digital"
-    document.querySelector(".clock-analog").style.display = "flex";
-    document.querySelector(".clock-digital").style.display = "none";
+    digitalClock.style.display = "none"
+    calendar.style.display = "none"
+    analogClock.style.display = "flex";
     launchAnalog();
     intervalDigital = setInterval(launchAnalog, 1000);
 }
 function addDigital(){
     clearInterval(intervalAnalog);
-    btnToggle.innerText = "Analog"
-    document.querySelector(".clock-analog").style.display = "none";
-    document.querySelector(".clock-digital").style.display = "flex";
+    calendar.style.display = "none";
+    analogClock.style.display = "none";
+    digitalClock.style.display = "flex";
     launchDigital();
     intervalAnalog = setInterval(launchDigital, 1000);
 }
-btnToggle.addEventListener('click', () => {
-    String(btnToggle.innerText) === "ANALOG" ? addAnalog() : addDigital();
+function addCalendar(){
+    clearInterval(intervalAnalog);
+    clearInterval(intervalDigital);
+    analogClock.style.display = "none";
+    digitalClock.style.display = "none";
+    calendar.style.display = "block";
+}
+
+digitalBtn.addEventListener("click", () => {
+    addDigital()
+    dropdownbtn.textContent = "Digital clock";
+})
+analogBtn.addEventListener("click", () => {
+    addAnalog()
+    dropdownbtn.textContent = "Analog clock";
+})
+calendarBtn.addEventListener("click", () => {
+    addCalendar()
+    dropdownbtn.textContent = "Calendar";
+})
+
+
+// calendar
+
+const calendarDays = document.querySelector(".calendar-days");
+const MonthYear = document.getElementById("month-year");
+const prevMonth = document.getElementById("prev-btn");
+const nextMonth = document.getElementById("next-btn");
+
+const date = new Date();
+let currentMonth = date.getMonth();
+let currentYear = date.getFullYear();
+let currentDay = date.getDate();
+
+function renderCalendar(month, year){
+    MonthYear.textContent = `${months[month]} ${year}`;
+    calendarDays.innerHTML = ``;
+
+    const firstMonthday = new Date(year, month, 1).getDay();
+    const offset = (firstMonthday === 0) ? 6 : firstMonthday - 1;
+    const countDaysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstNextMonthday = new Date(year, month + 1, 1).getDay();
+    const offsetnext = (firstNextMonthday === 0) ? 6 : firstNextMonthday - 1;
+    const prevMonthdaysCount = new Date(year, month, 0).getDate();
+
+    for(let i = prevMonthdaysCount - offset + 1; i <= prevMonthdaysCount; i++){
+        let itemDayPrev = document.createElement("div");
+        itemDayPrev.textContent = i;
+        itemDayPrev.classList.add("prevnext");
+        calendarDays.appendChild(itemDayPrev);
+    }
+    for(let i = 1; i <= countDaysInMonth; i++){
+        let itemDay = document.createElement("div");
+        itemDay.textContent = i;
+        if(i === currentDay && currentMonth === date.getMonth()){
+            itemDay.classList.add("currentday");
+        }
+        calendarDays.appendChild(itemDay);
+    }
+    for(let i = 1; i <= 7 - offsetnext; i++){
+        let itemDayNext = document.createElement("div");
+        itemDayNext.textContent = i;
+        itemDayNext.classList.add("prevnext");
+        calendarDays.appendChild(itemDayNext);
+    }
+}
+renderCalendar(currentMonth, currentYear);
+
+prevMonth.addEventListener("click", () => {
+    currentMonth--;
+    if(currentMonth < 0){
+        currentMonth = 11;
+        currentYear--;
+    }
+    renderCalendar(currentMonth, currentYear);
+})
+nextMonth.addEventListener("click", () => {
+    currentMonth++;
+    if(currentMonth > 11){
+        currentMonth = 0;
+        currentYear++;
+    }
+    renderCalendar(currentMonth, currentYear);
+})
+
+
+dropdownbtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdownlist.classList.toggle("show");
+})
+document.addEventListener("click", () => {
+    dropdownlist.classList.remove("show");
 })
